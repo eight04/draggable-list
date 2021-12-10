ba20eddfeb0fd1b1ac30a2b23874e70f546a8868
eight04 <eight04@gmail.com>
2021-12-10 21:51:42 +0800

    Change: target Chrome 55

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ const presets = [
     "@babel/env",
     {
       targets: {
-        chrome: "49"
+        chrome: "55"
       },
       // https://github.com/facebook/regenerator/issues/276
       include: ["transform-template-literals"],
