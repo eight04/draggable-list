85750fbb83d212d76ff4982dac0385eb9a48e0cb
eight04 <eight04@gmail.com>
2021-12-11 23:26:11 +0800

    Add: support scrollingContainer

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,35 +1,38 @@
-import {babel} from "@rollup/plugin-babel";
-import {terser} from "rollup-plugin-terser";
+import { babel } from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
 
-function config({output, plugins = []}) {
-  return {
-    input: "index.mjs",
-    output: {
-      sourcemap: true,
-      ...output
-    },
-    plugins: [
-      babel({
-        babelHelpers: "bundled"
-      }),
-      terser(),
-      ...plugins
-    ]
-  };
+export default {
+  input: "index.mjs",
+  output: [...matrix(
+    [
+      [{ format: "iife", name: "DraggableList" }, { format: "esm" }],
+      [() => ({ plugins: [terser()] }), {}],
+    ],
+    (r) => ({
+      file: `dist/draggable-list.${r.format}${r.plugins ? ".min" : ""}.js`,
+    })
+  )],
+  plugins: [
+    babel({
+      babelHelpers: "bundled",
+    }),
+  ],
+};
+
+function* matrix(rows, other) {
+  yield* search(0, {});
+
+  function* search(i, result) {
+    if (i >= rows.length) {
+      yield { ...result, ...other(result) };
+      return;
+    }
+    for (const data of rows[i]) {
+      const newData = {
+        ...result,
+        ...(typeof data === "function" ? data() : data),
+      };
+      yield* search(i + 1, newData);
+    }
+  }
 }
-
-export default [
-  config({
-    output: {
-      format: "iife",
-      name: "DraggableList",
-      file: "dist/draggable-list.iife.js"
-    }
-  }),
-  config({
-    output: {
-      format: "esm",
-      file: "dist/draggable-list.esm.js"
-    }
-  })
-];
